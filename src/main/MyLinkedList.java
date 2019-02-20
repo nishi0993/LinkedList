4ba6f94cab73a13a84b46fdf677ffd15b06d4c02
nishi0993 <nishi0993@gmail.com>
2019-02-19 22:03:18 -0800

    add main

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -34,4 +34,11 @@ public class MyLinkedList {
         this.tail=null;
         currentSize=0;
     }
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList=new MyLinkedList();
+    Node node=new Node(10,null);
+    //linkedList.insert(node);
+
+    }
 }
