511dc5165b42110c6b88b900c7da684d55d5716b
nishi0993 <nishi0993@gmail.com>
2019-02-19 21:56:02 -0800

    add getter setter1

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -4,4 +4,12 @@ public class MyLinkedList {
     private Node head;
     private Node tail;
     private int currentSize;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
 }
